c6649099dc55d933cbbda9e44b9af503ec4bbdc1
thebjoern <thebjoern>
2002-01-31 12:01:57 +0000

    increase version

--- mfq3/source/game/bg_public.h
+++ mfq3/source/game/bg_public.h
@@ -1,5 +1,5 @@
 /*
- * $Id: bg_public.h,v 1.36 2002-01-31 10:12:05 sparky909_uk Exp $
+ * $Id: bg_public.h,v 1.37 2002-01-31 12:01:57 thebjoern Exp $
 */
 
 // Copyright (C) 1999-2000 Id Software, Inc.
@@ -9,7 +9,7 @@
 // because games can change separately from the main system version, we need a
 // second version that must match between game and cgame
 
-#define	GAME_VERSION		"mfq3 v0.56"
+#define	GAME_VERSION		"mfq3 v0.56a"
 
 #define	DEFAULT_GRAVITY		800
 #define	GIB_HEALTH			-50
