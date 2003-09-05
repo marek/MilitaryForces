a947d7f0a741e91555d9de1d776303d6e43dad4e
Marek Kudlacz <marek@kudlacz.com>
2003-09-05 00:51:46 +0000

    MF_ClientSpawnAtPos (still needs work)  messing around for recharge vehicle changing

--- mfq3/source/game/mf_client.c
+++ mfq3/source/game/mf_client.c
@@ -1,5 +1,5 @@
 /*
- * $Id: mf_client.c,v 1.17 2003-08-06 18:10:21 thebjoern Exp $
+ * $Id: mf_client.c,v 1.19 2003-10-07 23:15:57 minkis Exp $
 */
 
 #include "g_local.h"
@@ -249,6 +249,276 @@ void MF_ClientSpawn(gentity_t *ent) {
 
 		} while ( 1 );
 	}
+
+	client->pers.teamState.state = TEAM_ACTIVE;
+
+	// toggle the teleport bit so the client knows to not lerp
+	// and never clear the voted flag
+	flags = ent->client->ps.eFlags & (EF_TELEPORT_BIT | EF_VOTED | EF_TEAMVOTED);
+	flags ^= EF_TELEPORT_BIT;
+
+	// save veh
+	savedNext = client->nextVehicle;
+
+	// clear everything but the persistant data
+	saved = client->pers;
+	savedSess = client->sess;
+	savedPing = client->ps.ping;
+	accuracy_hits = client->accuracy_hits;
+	accuracy_shots = client->accuracy_shots;
+	for ( i = 0 ; i < MAX_PERSISTANT ; i++ ) {
+		persistant[i] = client->ps.persistant[i];
+	}
+	// also save the predictable events otherwise we might get double or dropped events
+	for (i = 0; i < MAX_PS_EVENTS; i++) {
+		savedEvents[i] = client->ps.events[i];
+	}
+	eventSequence = client->ps.eventSequence;
+
+	memset (client, 0, sizeof(*client));
+
+	client->pers = saved;
+	client->sess = savedSess;
+	client->ps.ping = savedPing;
+	client->accuracy_hits = accuracy_hits;
+	client->accuracy_shots = accuracy_shots;
+	client->lastkilled_client = -1;
+
+	for ( i = 0 ; i < MAX_PERSISTANT ; i++ ) {
+		client->ps.persistant[i] = persistant[i];
+	}
+	for (i = 0; i < MAX_PS_EVENTS; i++) {
+		client->ps.events[i] = savedEvents[i];
+	}
+	client->ps.eventSequence = eventSequence;
+	// increment the spawncount so the client will detect the respawn
+	client->ps.persistant[PERS_SPAWN_COUNT]++;
+	client->ps.persistant[PERS_TEAM] = client->sess.sessionTeam;
+	client->ps.clientNum = index;
+
+	client->airOutTime = level.time + 25000;
+
+	client->ps.eFlags = flags;
+
+	ent->s.groundEntityNum = ENTITYNUM_NONE;
+	ent->client = &level.clients[index];
+	ent->takedamage = qtrue;
+	ent->inuse = qtrue;
+	ent->classname = "player";
+	ent->r.contents = CONTENTS_BODY;
+	ent->clipmask = MASK_PLAYERSOLID;
+	ent->die = Vehicle_Death;
+	ent->waterlevel = 0;
+	ent->watertype = 0;
+	ent->flags = 0;
+	ent->left = qfalse; //first projectile starts right
+	ent->bulletpos = 0;
+
+	client->vehicle = client->nextVehicle = savedNext;
+
+	// vehicles only stuff
+	if( vehIndex >= 0 && vehIndex < bg_numberOfVehicles ) {
+		// distribute the weapons
+		MF_getDefaultLoadoutForVehicle( vehIndex, &ent->loadout );
+//		G_AddEvent( ent, EV_GET_DEFAULT_LOADOUT, 0 );
+		ent->loadoutUpdated = qfalse;
+
+		Info_SetValueForKey( userinfo, "cg_vehicle", va( "%d", vehIndex ) );
+		trap_SetUserinfo( index, userinfo );
+		ClientUserinfoChanged( index );
+
+		// cat specific stuff
+		if( availableVehicles[vehIndex].cat & CAT_PLANE ) {
+			// check for landed spawnpoint
+			qboolean landed = qfalse;
+			trace_t	trace;
+			vec3_t	endpos;
+			gentity_t *test;
+			VectorCopy( spawn_origin, endpos );
+			endpos[2] -= 128;
+			trap_Trace (&trace, spawn_origin, NULL, NULL, endpos, ENTITYNUM_NONE, MASK_ALL );
+			if( trace.entityNum != ENTITYNUM_NONE ) {
+				test = &g_entities[trace.entityNum];
+				if( canLandOnIt(test) ) {
+					landed = qtrue;
+					spawn_origin[2] = test->r.maxs[2] - availableVehicles[vehIndex].mins[2] + 
+						availableVehicles[vehIndex].gearheight;
+				}			
+			}
+			MF_Spawn_Plane( ent, vehIndex, landed );
+		} else if( availableVehicles[vehIndex].cat & CAT_GROUND ) {
+			
+			trace_t	trace;
+			vec3_t	endpos;
+			gentity_t *test;
+			VectorCopy( spawn_origin, endpos );
+			endpos[2] -= 512;
+
+			trap_Trace (&trace, spawn_origin, NULL, NULL, endpos, ENTITYNUM_NONE, MASK_ALL );
+			if( trace.entityNum != ENTITYNUM_NONE ) {
+				test = &g_entities[trace.entityNum];
+					spawn_origin[2] = trace.endpos[2] - availableVehicles[vehIndex].mins[2] + 1;
+			}
+			MF_Spawn_GroundVehicle( ent, vehIndex );
+		} else if( availableVehicles[vehIndex].cat & CAT_HELO ) {
+			MF_Spawn_Helo( ent, vehIndex, qfalse );
+		} else if( availableVehicles[vehIndex].cat & CAT_LQM ) {
+			MF_Spawn_LQM( ent, vehIndex );
+		} else if( availableVehicles[vehIndex].cat & CAT_BOAT ) {
+			trace_t	trace;
+			vec3_t	endpos;
+			gentity_t *test;
+			VectorCopy( spawn_origin, endpos );
+			endpos[2] -= 512;
+
+			trap_Trace (&trace, spawn_origin, NULL, NULL, endpos, ENTITYNUM_NONE, MASK_ALL );
+			if( trace.entityNum != ENTITYNUM_NONE ) {
+				test = &g_entities[trace.entityNum];
+					spawn_origin[2] = trace.endpos[2];
+			}
+			MF_Spawn_Boat( ent, vehIndex );
+		}
+
+		VectorCopy (availableVehicles[vehIndex].mins, ent->r.mins);
+		VectorCopy (availableVehicles[vehIndex].maxs, ent->r.maxs);
+
+		// set health
+		handicap = atoi( Info_ValueForKey( userinfo, "handicap" ) );
+		if ( handicap < 1 || handicap > 100 ) {
+			handicap = 100;
+		}
+		client->pers.maxHealth = availableVehicles[vehIndex].maxhealth * handicap / 100;
+		client->ps.stats[STAT_MAX_HEALTH] = client->pers.maxHealth;
+
+		// fuel
+		client->ps.stats[STAT_FUEL] = client->ps.stats[STAT_MAX_FUEL] = availableVehicles[vehIndex].maxfuel;
+		client->ps.stats[STAT_LOCKINFO] = 0;
+		client->ps.timers[TIMER_FUEL] = level.time;
+
+		// which weapons do we have
+		for( i = WP_MACHINEGUN; i <= WP_FLARE; i++ ) {
+			if( availableVehicles[vehIndex].weapons[i] != WI_NONE ) {
+				client->ps.ammo[i] = client->ps.ammo[i+8] = availableVehicles[vehIndex].ammo[i] *
+					availableWeapons[availableVehicles[vehIndex].weapons[i]].numberPerPackage;
+			}
+		}
+
+		// select the first weapon
+		for( i = WP_MACHINEGUN; i <= WP_FLARE; i++ ) {
+			if( availableVehicles[vehIndex].weapons[i] != WI_NONE ) {
+				client->ps.weaponIndex = availableVehicles[vehIndex].weapons[i];
+				break;
+			}
+		}
+		if( client->ps.weaponIndex == WI_NONE ) {
+			client->ps.weaponIndex = availableVehicles[vehIndex].weapons[0];
+		}
+
+	} else { // spectator only stuff
+		VectorSet( ent->r.mins, -8, -8, -8 );
+		VectorSet( ent->r.maxs, 8, 8, 8 );
+		if( client->sess.sessionTeam != TEAM_SPECTATOR ) {
+			client->ps.pm_flags |= PMF_VEHICLESELECT;
+			client->ps.pm_type = PM_FREEZE;
+			client->vehicle = -1;
+		}
+	}
+
+	// health will count down towards max_health
+	ent->health = client->ps.stats[STAT_HEALTH] = client->ps.stats[STAT_MAX_HEALTH] + 25;
+
+	G_SetOrigin( ent, spawn_origin );
+	VectorCopy( spawn_origin, client->ps.origin );
+
+	// the respawned flag will be cleared after the attack and jump keys come up
+	client->ps.pm_flags |= PMF_RESPAWNED;
+
+	trap_GetUsercmd( client - level.clients, &ent->client->pers.cmd );
+	SetClientViewAngle( ent, spawn_angles );
+
+	if ( ent->client->sess.sessionTeam == TEAM_SPECTATOR ) {
+
+	} else {
+		G_KillBox( ent );
+		trap_LinkEntity (ent);
+	}
+
+	// reset radar
+	ent->client->ps.ONOFF |= ent->client->pers.lastRadar;
+
+	// don't allow full run speed for a bit
+	client->ps.pm_flags |= PMF_TIME_KNOCKBACK;
+	client->ps.pm_time = 100;
+
+	client->respawnTime = level.time;
+	client->inactivityTime = level.time + g_inactivity.integer * 1000;
+	client->latched_buttons = 0;
+
+	// set default animations
+	client->ps.vehicleAnim = 0;
+	client->ps.gunAngle = 0;
+	client->ps.turretAngle = 0;
+
+	if ( level.intermissiontime ) {
+		MoveClientToIntermission( ent );
+	} else {
+		// fire the targets of the spawn point
+		G_UseTargets( spawnPoint, ent );
+	}
+
+	// reset lockinfo
+	unlock( ent );
+
+	// run a client frame to drop exactly to the floor,
+	// initialize animations and other things
+	client->ps.commandTime = level.time - 100;
+	ent->client->pers.cmd.serverTime = level.time;
+	ClientThink( ent-g_entities );
+
+	// positively link the client, even if the command times are weird
+	if( ent->client->sess.sessionTeam != TEAM_SPECTATOR ) {
+		BG_PlayerStateToEntityState( &client->ps, &ent->s, qtrue );
+		VectorCopy( ent->client->ps.origin, ent->r.currentOrigin );
+		trap_LinkEntity( ent );
+	}
+
+	// run the presend to set anything else
+	ClientEndFrame( ent );
+
+	// clear entity state values
+	BG_PlayerStateToEntityState( &client->ps, &ent->s, qtrue );
+}
+
+
+void MF_ClientSpawnAtPos(gentity_t *ent, vec3_t	spawn_origin, vec3_t spawn_angles ) {
+	int		index;
+	gclient_t	*client;
+	int		i;
+	clientPersistant_t	saved;
+	clientSession_t		savedSess;
+	int		persistant[MAX_PERSISTANT];
+	gentity_t	*spawnPoint;
+	int		flags;
+	int		savedPing;
+//	char	*savedAreaBits;
+	int		accuracy_hits, accuracy_shots;
+	int		savedEvents[MAX_PS_EVENTS];
+	int		eventSequence;
+	char	userinfo[MAX_INFO_STRING];
+	int		vehIndex;
+	int		savedNext;
+	int		handicap;
+
+	index = ent - g_entities;
+	client = ent->client;
+
+	trap_GetUserinfo( index, userinfo, sizeof(userinfo) );
+
+	// vehicle index for next spawning
+	vehIndex = atoi( Info_ValueForKey( userinfo, "cg_nextVehicle" ) );
+
+	// find a spawn point
+	spawnPoint = ent;
 
 	client->pers.teamState.state = TEAM_ACTIVE;
 
